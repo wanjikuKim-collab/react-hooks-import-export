da60db1af0c9d628f6de3dc0ee735c3d270df1dc
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-10 09:41:14 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
 
+
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
--- src/parks/howManyParks.js
+++ src/parks/howManyParks.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 function howManyParks() {
   console.log("42 parks!");
 }
